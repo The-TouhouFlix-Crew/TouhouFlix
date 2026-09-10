31cd2c41de479666f3d821dec3fba9b46fe488e7
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-10 17:45:58 -0300

    Add Main class with FlxGame initialization

---
+++ source/Main.hx
@@ -0,0 +1,12 @@
+package;
+
+import openfl.display.Sprite;
+import flixel.FlxGame;
+
+class Main extends Sprite {
+	public function new() {
+		super();
+
+		addChild(new FlxGame(1280, 720, MenuState, 60, 60, true));
+	}
+}
